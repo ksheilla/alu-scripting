6ef36fa0b70b63fbef69f3c95854ad3b257777dc
ksheilla <s.ruvugabig@alustudent.com>
2025-02-17 18:38:36 +0200

    Update 1-main.py

--- api_advanced/1-main.py
+++ api_advanced/1-main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-1-main
+Script to test the top_ten function from the 1-top_ten module.
 """
+
 import sys
 
 if __name__ == '__main__':
+    # Import the top_ten function from the 1-top_ten module
     top_ten = __import__('1-top_ten').top_ten
+
+    # Check if a subreddit argument is provided
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
+        # Call the top_ten function with the provided subreddit
         top_ten(sys.argv[1])
